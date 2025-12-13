8ecab196d044129fd368912616213947d83b3ec3
John Doe <jeremi@daym0ns.com>
2025-12-13 18:58:04 +0100

    modified split lengtj

--- nvim/.config/nvim/lua/plugins/treesj.lua
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -2,7 +2,7 @@ return {
   'Wansmer/treesj',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('treesj').setup({})
+    require('treesj').setup({ max_join_length = 520 })
     vim.keymap.set('n', '<leader>m', require('treesj').toggle)
   end,
 }
