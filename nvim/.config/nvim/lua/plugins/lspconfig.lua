6b933d303836361857c7a89192ef6218cc5525a3
Your Name <jeremi@daym0ns.com>
2025-12-15 17:48:34 +0100

    tmux mouse and diagnostic signs changed

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -8,6 +8,13 @@ return {
 			vim.keymap.set("n", "grn", vim.lsp.buf.rename)
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 			vim.keymap.set("n", "K", vim.lsp.buf.hover)
+
+      vim.fn.sign_define("DiagnosticSignError", { text = ">>", texthl = "DiagnosticSignError" })
+      vim.fn.sign_define("DiagnosticSignWarn",  {text = ">>", texthl = "DiagnosticSignWarn"})
+      vim.fn.sign_define("DiagnosticSignInfo",  {text = ">>", texthl = "DiagnosticSignInfo"})
+      vim.fn.sign_define("DiagnosticSignHint",  {text = ">>", texthl = "DiagnosticSignHint"})
+
+      vim.diagnostic.config({ virtual_text = true, signs = true, update_in_insert = true })
 		end,
 	},
 	{
--- nvim/.config/nvim/lua/plugins/treesj.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'Wansmer/treesj',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  config = function()
-    require('treesj').setup({ max_join_length = 520 })
-    vim.keymap.set('n', '<leader>m', require('treesj').toggle)
-  end,
-}
