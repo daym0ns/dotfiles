1046f493cfb8d571cea60710ab4b564906c5d6b3
bruh <bruh@mac.lan>
2026-01-03 14:29:30 +0100

    made whichkey prettier

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -2,6 +2,29 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   config = function()
-    require("which-key").setup({ icons = { mappings = false } })
+    require("which-key").setup({
+      icons = {
+        mappings = false
+      },
+      win = {
+        no_overlap = true,
+
+        width = { min = 40, max = 75 },
+        height = { min = 15, max = 20 },
+
+        col = math.huge,
+        row = -2,
+
+        border = "rounded",
+        padding = { 1, 2 },
+        title = true,
+        title_pos = "center",
+        zindex = 1000,
+        bo = {},
+        wo = {
+          winblend = 10,
+        },
+      },
+    })
   end
 }
